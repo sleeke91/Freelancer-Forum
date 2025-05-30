d482e43d1b9fd14b24e1ba47427232ab2ba0bf3f
Samantha Leeke <samantha@Samanthas-MacBook-Pro.local>
2025-05-29 21:30:46 -0500

    honest attempt

--- index.js
+++ index.js
@@ -10,3 +10,62 @@ const NAMES = ["Alice", "Bob", "Carol", "Dave", "Eve"];
 const OCCUPATIONS = ["Writer", "Teacher", "Programmer", "Designer", "Engineer"];
 const PRICE_RANGE = { min: 20, max: 200 };
 const NUM_FREELANCERS = 100;
+
+// Write a function that returns a freelancer object with a randomly generated name, occupation, and rate according to the provided constants.
+const makeFreelancerObject = ()=>{
+    const freeLancer = {}
+    // get a randomNum for the names Array
+    const randomNameNum = Math.floor(Math.random()*NAMES.length)
+    const randomName = NAMES[randomNameNum]
+    freeLancer.name = randomName
+    // get a randomNum for the occupations Array
+    const randomOccNum = Math.floor(Math.random()*OCCUPATIONS.length)
+    const randomOccupation = OCCUPATIONS[randomOccNum]
+    freeLancer.occupation = randomOccupation
+    // get a randomNum for the Price_Range Array *****STUCK*****
+    const randomRateNum = Math.floor(Math.random()*PRICE_RANGE.length)
+    const randomRate = PRICE_RANGE[randomRateNum]
+    freeLancer.rate = randomRate
+    return freeLancer
+}
+console.log(makeFreelancerObject())
+
+// Initialize a state variable to an array of `NUM_FREELANCERS` freelancer objects.
+function createRandomFreelancersArr(){
+    const freelancersArray = []
+    for(let i=0;i<100;i++){
+      const newFreelancer = makeFreelancerObject() //calls on the previous function created above, running this function x100
+      freelancersArray.push(newFreelancer)
+    }
+    return freelancersArray
+  }
+  const freelancerStateArray = createRandomFreelancersArr()
+  console.log(freelancerStateArray)
+
+// Write a function that returns the average rate of all freelancers in state.
+function getSum(freelancerStateArray) {
+    let sum = 0
+    for(let i=0; i<freelancerStateArray.PRICE_RANGE;i++){
+     sum = sum + freelancerStateArray.PRICE_RANGE[i]
+    }
+    return sum
+  }
+
+function getaverageRate (freelancerStateArray){
+    const sum = getSum(freelancerStateArray)
+    const length = NUM_FREELANCERS
+  
+    return (sum/length)
+}
+
+console.log(getaverageRate())
+
+// Use that function to initialize a state variable which will store the average rate of all freelancers.
+
+// Write a component function to represent a single freelancer.
+
+// Write a component function to represent an array of freelancers.
+
+// Write a component function to represent the average rate of all freelancers.
+
+// Write and call a render function that will mount the application onto the document.
